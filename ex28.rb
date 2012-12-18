9533c384fe85ba5bc493bed255b9b4deda8d92f0
sakishum <sakishum1118@gmail.com>
2012-12-18 08:03:41 +0800

    While-loop
    basic while-loop exercises

---
+++ ex28.rb
@@ -0,0 +1,20 @@
+
+def while_loop(max_size, addition)
+	numbers = []
+	i = 0
+	while i < max_size
+		puts "At the top i is #{i}"
+		numbers.push(i)
+
+		i = i + addition
+		puts "Numbers now: #{numbers}"
+		puts "At the bottom i is #{i}"
+	end
+
+	puts "The numbers:"
+
+	for num in numbers
+		puts num
+	end
+end
+while_loop(6, 2)
